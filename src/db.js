172e5420df6f8b764b777b2306355e64ddde919e
vikram1vicky@gmail.com <vikram1vicky@gmail.com>
2020-03-08 17:52:49 +0530

    commented options for DB connection

--- src/db.js
+++ src/db.js
@@ -7,10 +7,10 @@ const { DB_BUFFER_COMMANDS, DB_CONNECTION_TIMEOUT_MS, DB_KEEP_ALIVE,
 
 export const initDatabase = (cb) => {
     mongoose.connect(process.env.MONGODB_URI || DB_PATH || 'mongodb://localhost:27017/test', {
-        bufferCommands: DB_BUFFER_COMMANDS === 'true',       // Turn off the bufferCommands to disable buffering
-        connectTimeoutMS: parseInt(DB_CONNECTION_TIMEOUT_MS),
-        socketTimeoutMS: parseInt(DB_SOCKET_TIMEOUT_MS),
-        keepAlive: DB_KEEP_ALIVE === 'true',
+        // bufferCommands: DB_BUFFER_COMMANDS === 'true',       // Turn off the bufferCommands to disable buffering
+        // connectTimeoutMS: parseInt(DB_CONNECTION_TIMEOUT_MS),
+        // socketTimeoutMS: parseInt(DB_SOCKET_TIMEOUT_MS),
+        // keepAlive: DB_KEEP_ALIVE === 'true',
         // reconnectTries: parseInt(DB_RECONNECT_TRIES),
         useNewUrlParser: true,
         // dbName: 'heroku_1rgknm91' || process.env.DB_NAME,
